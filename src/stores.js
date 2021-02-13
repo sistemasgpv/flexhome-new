1da37e1c8f8b400d1e2bad3af01fd7c49c3fb1c2
Dan <dan784@gmail.com>
2021-02-13 15:42:08 +0200

    first

---
+++ src/stores.js
@@ -0,0 +1,31 @@
+import { writable } from "svelte/store";
+import axios from "axios";
+
+export const viviendas = writable([]);
+
+export const opciones = writable(null);
+
+export const categorias = writable({
+  diversion: false,
+  seguridad: false,
+  confort: false,
+  eficiencia: false,
+});
+
+export function getViviendas() {
+  axios.get("https://en57ds8aebutpuq.m.pipedream.net").then((res) => {
+    viviendas.set(res.data);
+    let v = res.data[0].fields.Nombre;
+    getOpciones(v);
+  });
+}
+
+export function getOpciones(vivienda) {
+  axios
+    .get(`https://enombb1z99rtf6o.m.pipedream.net?vivienda=${vivienda}`)
+    .then((res) => {
+      opciones.set(res.data);
+    });
+}
+
+getViviendas();
