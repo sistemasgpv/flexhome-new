76706d7c7395ac61510e2dc4653bff85092de8a1
Dan <dan784@gmail.com>
2021-02-14 20:54:54 +0200

    WIP

--- src/Opciones.svelte
+++ src/Opciones.svelte
@@ -1,8 +1,18 @@
 <script>
-  import { opciones } from "./stores.js";
+  import { atributos, opcionesByAtributo } from "./stores.js";
 </script>
 
-<h1>Opciones</h1>
+<div class="opciones-container">
+  {#each $atributos as atributo}
+    <div class="atributo-title">
+      {atributo.fields.Nombre}
+      {#each Object.keys($opcionesByAtributo) as opcione}
+        {opcione}
+      {/each}
+    </div>
+    <div class="opciones" />
+  {/each}
+</div>
 
 <style>
   /* your styles go here */
--- src/stores.js
+++ src/stores.js
@@ -1,11 +1,17 @@
 import { writable } from "svelte/store";
 import axios from "axios";
 
+export const proyectos = writable([]);
+
+export const modelos = writable([]);
+
 export const viviendas = writable([]);
 
-export const opciones = writable(null);
+export const opciones = writable([]);
 
 export const atributos = writable([]);
+
+export const opcionesByAtributo = writable({});
 
 export const categorias = writable({
   diversion: false,
@@ -14,11 +20,24 @@ export const categorias = writable({
   eficiencia: false,
 });
 
-export function getViviendas() {
+export function getAtributos() {
+  axios.get("https://enl4yiidhnuij8n.m.pipedream.net").then((res) => {
+    atributos.set(res.data);
+  });
+}
+
+export function getProyectos() {
   axios.get("https://en57ds8aebutpuq.m.pipedream.net").then((res) => {
-    viviendas.set(res.data);
-    let v = res.data[0].fields.Nombre;
-    getOpciones(v);
+    proyectos.set(res.data);
+    getModelos();
+  });
+}
+
+export function getModelos() {
+  axios.get("https://enw9gnpjz0b6y3s.m.pipedream.net").then((res) => {
+    modelos.set(res.data);
+    debugger;
+    getOpciones(res.data[0].fields.Nombre);
   });
 }
 
@@ -27,7 +46,20 @@ export function getOpciones(vivienda) {
     .get(`https://enombb1z99rtf6o.m.pipedream.net?vivienda=${vivienda}`)
     .then((res) => {
       opciones.set(res.data);
+
+      let byAtr = {};
+      res.data.map((o) => {
+        let atr = o.fields.atributo_nombre;
+        if (!byAtr[atr]) {
+          byAtr[o.fields.atributo_nombre] = [o];
+        } else {
+          byAtr[o.fields.atributo_nombre].push(o);
+        }
+      });
+      console.log(byAtr);
+      opcionesByAtributo.set(byAtr);
     });
 }
 
-getViviendas();
+getAtributos();
+getProyectos();
