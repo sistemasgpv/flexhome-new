5899cd0e5b4b459f859e5723133ef9ad53c93ea8
Dan <dan784@gmail.com>
2021-02-28 11:08:51 +0200

    wip

--- src/SelectModelo.svelte
+++ src/SelectModelo.svelte
@@ -12,6 +12,15 @@
   import SideItem from "./SideItem.svelte";
 
   let showSelect = true;
+
+  // let loadingModelos = true;
+  let loadingOpciones = false;
+
+  $: loading = $proyectos.length == 0 || $modelos.length == 0;
+
+  opciones.subscribe((ops) => {
+    if (ops.length > 0) loadingOpciones = false;
+  });
 </script>
 
 <div class="mi-casa">
@@ -44,54 +53,59 @@
 </div>
 
 <!-- Overlay modal-->
-{#if showSelect || ($opciones && $opciones.length == 0)}
+{#if showSelect || loading || loadingOpciones}
   <div
     class="select-modal"
     on:click={() => {
-      showSelect = false;
+      if (!loadingOpciones) showSelect = false;
     }}
     transition:fade={{ duration: 150 }}
   >
-    <div class="select-modal-bg">
-      <div class="modal-title">Select Project</div>
-      <div class="select-proyectos">
-        {#each $proyectos as proyect}
-          <div
-            class="select-proyect"
-            class:selected={proyect == $currentSelection.proyect}
-          >
-            <div class="proyect-img">
-              {proyect.fields.Nombre} - Image
+    {#if loadingOpciones || loading}
+      <div class="loading">loading</div>
+    {:else}
+      <div class="select-modal-bg">
+        <div class="modal-title">Select Project</div>
+        <div class="select-proyectos">
+          {#each $proyectos as proyect}
+            <div
+              class="select-proyect"
+              class:selected={proyect == $currentSelection.proyect}
+            >
+              <div class="proyect-img">
+                {proyect.fields.Nombre} - Image
+              </div>
+              <div class="proyect-title">
+                {proyect.fields.Nombre}
+              </div>
             </div>
-            <div class="proyect-title">
-              {proyect.fields.Nombre}
-            </div>
-          </div>
-        {/each}
-      </div>
+          {/each}
+        </div>
 
-      <div class="modal-title">Select Model</div>
-      <div class="select-modelos">
-        {#each $modelos as modelo}
-          <div
-            class="select-modelo"
-            class:selected={modelo == $currentSelection.modelo}
-            on:click|stopPropagation={() => {
-              $currentSelection.modelo = modelo;
-              getOpciones(modelo.fields.Nombre);
-              showSelect = false;
-            }}
-          >
-            <div class="modelo-img">
-              {modelo.fields.Nombre} - Image
+        <div class="modal-title">Select Model</div>
+        <div class="select-modelos">
+          {#each $modelos as modelo}
+            <div
+              class="select-modelo"
+              class:selected={modelo == $currentSelection.modelo}
+              on:click|stopPropagation={() => {
+                $currentSelection.modelo = modelo;
+                getOpciones(modelo.fields.Nombre);
+                showSelect = false;
+                loadingOpciones = true;
+              }}
+            >
+              <div class="modelo-img">
+                {modelo.fields.Nombre} - Image
+              </div>
+              <div class="modelo-title">
+                {modelo.fields.Nombre}
+              </div>
             </div>
-            <div class="modelo-title">
-              {modelo.fields.Nombre}
-            </div>
-          </div>
-        {/each}
+          {/each}
+        </div>
       </div>
-    </div>
+    {/if}
   </div>
 {/if}
 
